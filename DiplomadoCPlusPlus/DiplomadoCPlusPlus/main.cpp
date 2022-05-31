562eb8531d09219ee69e812931d69e13e12bd2e3
Roberto Charreton <61713617+robertocharretonkaplun@users.noreply.github.com>
2022-05-31 18:28:59 -0500

    Clases - Vector explanation
    Added:
    - Explanation for vector structure and cin acion.

--- DiplomadoCPlusPlus/DiplomadoCPlusPlus/main.cpp
+++ DiplomadoCPlusPlus/DiplomadoCPlusPlus/main.cpp
@@ -25,7 +25,7 @@ int main() {
   // Una variable es un dato que pueda cambiar conforme pase el tiempo.
 
   // Variables por defecto en C++
-  
+
   // int -> Maneja datos enteros. Ejemplo = 10
   int edad = 23;
   // float -> Maneja datos decimales. Ejemplo = 1.6f
@@ -47,12 +47,12 @@ int main() {
 
   // Cuanto pesa 1 byte en memoria: 8 bits;
   // 1 byte = 8 bits
-  
+
   // Int = sizeOf(4 byte) -> 4 bytes son 32 bits.
   cout << "int: " << sizeof(int) << endl;
   int intToBit = sizeof(int) * 8;
   cout << "int to bit: " << intToBit << endl;
-  
+
   // float = sizeOf() -> 4 bytes son 32 bits.
   cout << "float: " << sizeof(float) << endl;
   int floatToBit = sizeof(float) * 8;
@@ -70,14 +70,14 @@ int main() {
 
   // Constantes en C++
   const int IDVersion = 342;
-  
+
 
   // Que es una variable constante?
   // Es una variable la cual tiene un valor que nunca cambiara.
-  
+
   // Porque debo utilizar una variable constante?
   // Porque generamos valores estandarizados que no se van a modificar.
-  
+
   // Existe algun riesgo 
   // Inyeccion de datos a la memoria RAM.
   cout << "IDVersion: " << IDVersion << endl;
@@ -114,19 +114,19 @@ int main() {
   // Que es un arreglo: Un conjunto de datos ordenados.
   // Ejemplo: frutas: Pera, manzana, uvas, duraznos . . .
   // Estructura General: Tipo-nombre-Tamano=Contenido
-  
+
   // ARREGLOS UNIDIMENCIONALES
 
   // Inicializacion 
   string frutas[10] = { "Pera", "Manzana", "Uvas", "Duraznos", "Platano" };
-  string colores[] = {"Rojo", "Azul", "Verde"};
+  string colores[] = { "Rojo", "Azul", "Verde" };
   int edades[10];
 
   float calificaciones[5] = { 6.6f, 5.0f, 8.0f, 7.0f, 10.0f };
   // Modificacion de una posicion en el arreglo
   calificaciones[0] = 9.0f;
-  int sizeOfCalificaciones = sizeof(calificaciones)/ sizeof(float);
-
+  int sizeOfCalificaciones = sizeof(calificaciones) / sizeof(float);
+   
   cout << "Size in memory from calificaciones: " << sizeof(calificaciones) << endl;
   cout << "Size in memory from float: " << sizeof(float) << endl;
   cout << "Size of calificaciones: " << sizeOfCalificaciones << endl;
@@ -146,7 +146,7 @@ int main() {
                                 {"Alvaro" ,"Roberto" , "Fernandad"},
                                 {"Victor" , "Saul", "Pedro"       },
                                 {"Alfredo", "Gabriela", "Mario"   }
-                              };
+  };
   // Modificacion de un arreglo bidimensional
   estudiantes1[0][2] = "Fernanda";
   string estudianteprueba = estudiantes1[0][2];
@@ -157,9 +157,57 @@ int main() {
   // VECTORES - Incluir la libria de vector
   // Definir un vector
   vector<string> contrasenas;
-  contrasenas.push_back("lsdknhfcs");
-  contrasenas.push_back("ELONMusk");
-  cout << "Contrasena: " << contrasenas[0] << endl;
+  contrasenas.push_back("Fernanda"); // 0
+  contrasenas.push_back("Gaby");     // 1
+  contrasenas.push_back("Oswaldo");  // Se removio a oswaldo
+
+  for (int i = 0; i < contrasenas.size(); i++)
+  {
+    cout << "Contrasena: " << contrasenas[i] << endl;
+
+  }
+
+  contrasenas.pop_back();
+  contrasenas.push_back("Alvaro"); // 2
+  contrasenas.push_back("Mario");  // 3
+  contrasenas.push_back("Victor");  // 4
+  contrasenas.push_back("Roberto"); // 5
+  for (int i = 0; i < contrasenas.size(); i++)
+  {
+    cout << "Contrasena: " << contrasenas[i] << endl;
+
+  }
+
+  int datosAConsultar = 5;
+  if (contrasenas.size() > datosAConsultar)
+  {
+    cout << "Posicion 5: " << contrasenas[5] << endl;
+  }
+
+  // 
+  //cout << "Elemento Posicion 0: " << contrasenas[6] << endl;
+ // cout << "Elemento Posicion 0: " << contrasenas.at(6) << endl;
+
+  vector<string>::iterator iter;
+  iter = contrasenas.begin();
+  //string tempPassword = contrasenas[0];
+  vector<string>::iterator tempPassword = contrasenas.begin();
+
+  int jovenes[]={1,2,4};
+
+  vector<int> adultos;
+  adultos.reserve(10000);
+  adultos.resize(10000);
+  adultos.clear();
+
+  string nombre2;
+  cout << "Ingresa tu nombre: ";
+  cin >> nombre2;
+
+
+  cout << "Tu nombre es: " << nombre2 << endl;
+
+
   return 0;
 }
 
