46fe795be432e8a52a24ef5169330c0fc6213ba9
Roberto Charreton <61713617+robertocharretonkaplun@users.noreply.github.com>
2022-06-02 18:33:12 -0500

    Clases - Implementacion de Operadores y condicionales
    Added:
    - Explicacion de operadores: Asignacion, Aritmeticos y relacionales
    - Explicacion de condicional if

--- DiplomadoCPlusPlus/DiplomadoCPlusPlus/main.cpp
+++ DiplomadoCPlusPlus/DiplomadoCPlusPlus/main.cpp
@@ -126,7 +126,7 @@ int main() {
   // Modificacion de una posicion en el arreglo
   calificaciones[0] = 9.0f;
   int sizeOfCalificaciones = sizeof(calificaciones) / sizeof(float);
-   
+
   cout << "Size in memory from calificaciones: " << sizeof(calificaciones) << endl;
   cout << "Size in memory from float: " << sizeof(float) << endl;
   cout << "Size of calificaciones: " << sizeOfCalificaciones << endl;
@@ -193,13 +193,13 @@ int main() {
   //string tempPassword = contrasenas[0];
   vector<string>::iterator tempPassword = contrasenas.begin();
 
-  int jovenes[]={1,2,4};
+  int jovenes[] = { 1,2,4 };
 
   vector<int> adultos;
   adultos.reserve(10000);
   adultos.resize(10000);
   adultos.clear();
-
+  /*
   string nombre2;
   cout << "Ingresa tu nombre: ";
   cin >> nombre2;
@@ -216,6 +216,180 @@ int main() {
   cin >> b;
   r = a + b;
   cout << "El resultado de la suma de " << a << "+" << b << " es: " << r << endl;
+  */
+
+  /* MODULO 5: OPERADORES */
+  cout << "---- OPERADORES ----" << endl;
+
+  // Operador: Es un elemento del programa que se aplica a uno o verios operandos 
+  // en una instruccion.
+
+  // OPERADORES DE ASIGNACION: Permiten la asignacion de  un valor a una variable.
+
+  // Operador de asignacion basica: '='. Ejemplo: X = 6; Resultado es X vale 6.
+  int X = 6;
+  cout << "Resultado de Asignacion Asignacion: " << X << endl;
+  // Operador de asignacion producto: '*='. Ejemplo: X *= 5; Resultado es X vale 30.
+  X *= 6;
+  cout << "Resultado de Asignacion Producto: " << X << endl;
+  // Operador de asignacion Division: '/=/. Ejemplo X /= 2; Resultado es X vale 18.
+  X /= 2;
+  cout << "Resultado de Asignacion Division: " << X << endl;
+  // Operador de Asignacion Suma: '+='. Ejemplo: X+= 4; Resultado es X vale 22
+  X += 4;
+  cout << "Resultado de Asignacion Suma: " << X << endl;
+  // Operador de Asignacion Resta: '-='. Ejemplo: X-= 2; Resultado es X vale 20 
+  X -= 2;
+  cout << "Resultado de Asignacion Resta: " << X << endl;
+  // Operador de Asignacion Modulo: '%='. Ejemplo: X %= 5; Resultado es X vale 1 
+  X = 6;
+  X %= 5;
+  cout << "Resultado de Asignacion Modulo: " << X << endl;
+
+  // OPERADORES ARITMETICOS: Se utilizan para calculos y operaciones con numeros
+  // reales y punteros.
+  int Y = 0;
+
+  // Operador de Suma: '+'. Ejemplo; Y = 5 + 3; Resultado Y vale 8
+  Y = 5 + 3;
+  cout << "Resultado Aritmetico Suma: " << Y << endl;
+  // Operador de Restas: '-'. Ejemplo; Y = 5 - 3; Resultado Y vale 2
+  Y = 5 - 3;
+  cout << "Resultado Aritmetico Resta: " << Y << endl;
+  // Operador de Multiplicacion: '*'. Ejemplo; Y = 5 * 3; Resultado Y vale 15
+  Y = 5 * 3;
+  cout << "Resultado Aritmetico Multiplicacion: " << Y << endl;
+  // Operador de Division: '/'. Ejemplo; Y = 5 / 3; Resultado Y vale 1
+  Y = 5 / 3;
+  cout << "Resultado Aritmetico Division: " << Y << endl;
+  // Operador de Modulo: '%'. Ejemplo; Y = 5 % 2; Resultado Y vale 1
+  Y = 5 % 2;
+  cout << "Resultado Aritmetico Modulo: " << Y << endl;
+  // Operador de Incremento: '++'. Ejemplo; Y = 5, Y++; Resultado Y vale 6
+  Y = 5;
+  Y++;
+  cout << "Resultado Aritmetico Incremento: " << Y << endl;
+  // Operador de Decremento: '--'. Ejemplo; Y = 5, Y--; Resultado Y vale 4
+  Y = 5;
+  Y--;
+  cout << "Resultado Aritmetico Decremento: " << Y << endl;
+
+  // OPERADORES RELACIONALES: Se les denomina operadores binarios logicos y de
+  // comparacion, normalmente, se utilizan para comprobar la veracidad o falsedad
+  // de ciertas condiciones.
+
+  // Operador relacional igualacion: '=='l Ejemplo:
+  int C = 5;
+  int D = 6;
+  if (C == D)
+  {
+    cout << "C es igual que D" << endl;
+  }
+  else
+  {
+    cout << "C no es igual que D" << endl;
+  }
+
+  // Operador relacional Menor: '<'; Ejemplo:
+  if (C < D)
+  {
+    cout << "C es menor que D" << endl;
+  }
+  else 
+  {
+    cout << "C no es menor que D" << endl;
+  }
+  // Operador relacional Mayor: '>'; Ejemplo:
+  if (C > D)
+  {
+    cout << "C es mayor que D" << endl;
+  }
+  else
+  {
+    cout << "C no es mayor que D" << endl;
+  }
+
+
+
+  // CONDICIONALES: Son porciones de codigo que se encargan de validar una comparacion
+  // de datos
+  // Ejemplo
+  int val = 3;
+  if (val == 2)
+  {
+    cout << "Wow 2 si es 2" << endl;
+  }
+  else 
+  {
+    cout << "Naaa " << val << " no es igual a 2" << endl;
+  }
+
+  // Accion: Roberto tu mama te manda por las tortillas.
+  // Reaccion: Roberto va a ir por las tortillas.
+  // Conclusion: Roberto fue por las tortillas.
+
+  // Accion: Roberto tu mama te manda por las tortillas si tienes dinero.
+  // Reaccion: Si y solo si Roberto tiene dinero para las tortillas el ira por
+  // tortillas.
+  // Conclusion 1: Si Roberto tuvo dinero, el fue por tillas.
+  // Conclusion 2: Si Roberto no tuvo dinero, el no fue por las tortillas.
+
+  // Accion: Roberto, si tu mama te manda por las tortilla y tienes dinero,
+  // vas a ir a preguntar si tiene tortillas, si tienen tortillas compraras
+  // un kilo.
+  // Condicion 1: Si mi mama me manda por las tortillas.
+  // Condicion 2: Tengo dinero?.
+  // Condicion 3: Mi mama me manda por las tortillas y tengo dinero.
+  // Accion: Pregruntar si en la tortilleria tienen tortillas?
+  // Condicion 4: Mi mama me manda por las tortillas, tengo dinero, hay tortillas.
+  // Conclusion: Voy a ir a la tortilleria si mi mama me manda y tengo dinero, 
+  // preguntando si tiene tortillas y asi poder comprar el kilo de tortillas.
+  bool MeMandaronPorTortillas = true;
+  bool tengoDinero = true;
+  bool voyAIrPorTortillas = false;
+  bool TienenTortillasEnLaTortilleria= true;
+  bool tengoLasTortillas = false;
+
+  if (MeMandaronPorTortillas == true)
+  {
+    cout << "Me mandaron por las tortillas :)" << endl;
+    if (tengoDinero == true)
+    {
+      cout << "Tengo Dinero :)" << endl;
+      voyAIrPorTortillas = true;
+      if (TienenTortillasEnLaTortilleria == true)
+      {
+        cout << "Tienen tortillas en la tortilleria :)" << endl;
+        if (TienenTortillasEnLaTortilleria == true && tengoDinero == true )
+        {
+          cout << "Comprare mis tortillas:)" << endl;
+          tengoLasTortillas = true;
+        }
+        else {
+          cout << "No habia tortillas o dinero :(" << endl;
+        }
+      }
+      else {
+        cout << "No tienen tortillas en la tortilleria :(" << endl;
+      }
+    }
+    else {
+      cout << "No tengo Dinero :(" << endl;
+      voyAIrPorTortillas = false;
+    }
+  }
+  else
+  {
+    cout << "No me mandaron por las tortillas :(" << endl;
+  }
+
+  if (tengoLasTortillas == true)
+  {
+    cout << "YEY tengo mis tortillas" << endl;
+  }
+  else {
+    cout << "Rayos no tengo mis tortillas" << endl;
+  }
   return 0;
 }
 
